e070d4cec83afc37ac84bc9afb7ce9928524cc71
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-07-08 19:03:47 +0530

    cpp days

--- 12.cpp
+++ 12.cpp
@@ -1,1 +1,19 @@
-// palindrome
+// palindrome number
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,rev=0,org;
+    cout<<"Enter a number: ";
+    cin>>n;
+    org = n;
+    for(i=0;n!=0;i++){
+        rev = n%10 + rev*10;
+        n/=10;
+    }
+    if(org==rev){
+        cout<<"Given number is palindromic";
+    }else{
+        cout<<"Given number is non palindromic";
+    }
+    return 0;
+}
