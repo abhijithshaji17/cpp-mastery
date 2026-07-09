b3df19737e7a2ec623c7fb7ca3493ebb4f696fa4
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-07-09 19:15:07 +0530

    cpp days

--- 16.cpp
+++ 16.cpp
@@ -1,3 +1,24 @@
 // matrix
-
+#include<iostream>
+using namespace std;
+int main(){
+    int a[10][10],m,n,i,j;
+    cout<<"Enter the no. of rows and columns of matrix: ";
+    cin>>m>>n;
+    cout<<"Enter matrix elements:\n";
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            cout<<"Element "<<i+1<<": ";
+            cin>>a[i][j];
+        }
+    }
+    cout<<"Matrix:\n";
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            cout<<a[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
 
