08705c08d6565e0762aee4c369385a0bf7bf8246
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-07-09 18:59:16 +0530

    cpp days

--- 15.cpp
+++ 15.cpp
@@ -1,47 +1,56 @@
-// matrix
-/*Calculator using Switch-case*/
-
-#include <iostream>
-
-int main()
-{
-    int op;
-    double n1, n2, ans;
-    
-    std::cout << "----Menu Driven Calculator----\n";
-    std::cout << "Enter the first number:\n";
-    std::cin >> n1;
-    std::cout << "Enter the second number:\n";
-    std::cin >> n2;
-    
-    std::cout << "Select the desired operation below:\n";
-    std::cout << "1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n";
-    std::cin >> op;
-    
-    switch(op) {
-        case 1: 
-            ans = n1 + n2;
-            std::cout << "Sum = " << ans << "\n";
-            break;
-        case 2: 
-            ans = n1 - n2;
-            std::cout << "Difference = " << ans << "\n";
-            break;
-        case 3: 
-            ans = n1 * n2;
-            std::cout << "Product = " << ans << "\n";
-            break;
-        case 4:
-            if (n2 != 0) {
-                ans = n1 / n2;
-                std::cout << "Quotient = " << ans << "\n";
-            } else {
-                std::cout << "Error: Division by zero not possible!\n";
+// calculator switchcase program with class and objects
+#include<iostream>
+using namespace std;
+class Calculator{
+public: 
+        void getInput(){
+            int op;
+            double n1,n2,ans;
+            cout<<"----Menu Driven Calculator----\n";
+            cout<<"Enter the first number: ";
+            cin>>n1;
+            cout<<"Enter the second number: ";
+            cin>>n2;
+            cout<<"Select the desired operation below:\n";
+            cout<<"1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n";
+            cin>>op;
+        }
+        void calculate(){
+            int op;
+            double n1,n2,ans;
+            switch(op){
+                case 1:
+                    ans=n1+n2;
+                    cout<<"Sum = "<<ans;
+                    break;
+                case 2:
+                    ans=n1-n2;
+                    cout<<"Difference = "<<ans;
+                    break;
+                case 3:
+                    ans=n1*n2;
+                    cout<<"Product = "<<ans;
+                    break;
+                case 4:
+                if(n2!=0){
+                    ans=n1/n2;
+                    cout<<"Quotient = "<<ans;
+                }else{
+                    cout<<"Error: division by zero!";
+                }
+                    break;
+                default:
+                    cout<<"Invalid choice.";
             }
-            break;
-        default:
-            std::cout << "Invalid choice.\n";
-    }
-    
+        }
+};
+int main(){
+    Calculator calc;
+    calc.getInput();
+    calc.calculate();
     return 0;
 }
+
+    
+
+
