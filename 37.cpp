d8aa9a06af9292593e12a6462f22ccd935d1a7a1
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-08-12 23:38:56 +0530

    cpp days

--- 37.cpp
+++ 37.cpp
@@ -15,3 +15,37 @@ int partition(int a[], int lb, int ub){
     swap(a[i+1],a[ub]);
     return i+1;
 }
+
+void quicksort(int a[], int lb, int ub){
+    int p;
+    if(lb<ub){
+        p = partition(a,lb,ub);
+        quicksort(a,lb,p-1);
+        quicksort(a,p+1,ub);
+    }
+}
+
+int main(){
+    int a[20],i,n;
+    cout<<"Enter the no. of elements: ";
+    cin>>n;
+    cout<<"Enter elements:\n";
+    for(i=0;i<n;i++){
+        cout<<"Element "<<i+1<<": ";
+        cin>>a[i];
+    }
+    cout<<"\nUnsorted Array:\n";
+    cout<<"[";
+    for(i=0;i<n;i++){
+        cout<<" "<<a[i];
+    }
+    cout<<"]\n";
+    quicksort(a,0,n-1);
+    cout<<"\nSorted Array:\n";
+    cout<<"[";
+    for(i=0;i<n;i++){
+        cout<<" "<<a[i];
+    }
+    cout<<"]\n";
+    return 0;
+}
