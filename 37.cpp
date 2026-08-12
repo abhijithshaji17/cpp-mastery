fe5eca06824565d6283bfe022e3c0fd9c16b8003
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-08-12 23:07:03 +0530

    cpp days

--- 37.cpp
+++ 37.cpp
@@ -1,2 +1,17 @@
-// Selection sort
-// ifykyk
+// Quick sort - lomuto
+#include<iostream>
+using namespace std;
+
+int partition(int a[], int lb, int ub){
+    int i,j,pivot;
+    pivot = a[ub];
+    i = lb - 1;
+    for(j=lb;j<ub;j++){
+        if(a[j]<=pivot){
+            i++;
+            swap(a[i],a[j]);
+        }
+    }
+    swap(a[i+1],a[ub]);
+    return i+1;
+}
